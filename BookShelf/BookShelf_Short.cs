f71e9c19ce23aeaeb6a8e0c4041d47c717ec67d8
Remigiusz Ruszkiewicz <r.ruszkiewicz@microsoft.wsei.edu.pl>
2019-10-21 19:02:08 +0200

    Add project files.

---
+++ BookShelf/BookShelf_Short.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShelf
+{
+    public class BookShelf_Short : BookShelf
+    {
+        public BookShelf_Short()
+        {
+            Name = "Niski";
+        }
+    }
+}
---
+++ BookShelf/BookShelf_Tall.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShelf
+{
+    class BookShelf_Tall : BookShelf
+    {
+        public BookShelf_Tall()
+        {
+            Name = "Wysoki";
+        }
+    }
+}
